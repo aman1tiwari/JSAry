71c50e80a9b8b89bea8ebfc9a6746626bef8bc3d
Aman Tiwari <amantiwari@cedcommerce.com>
2022-04-07 19:17:21 +0530

    Check2

--- src/script.js
+++ src/script.js
@@ -31,59 +31,131 @@ function add() {
       element.Price +
       "</td><td>" +
       element.Quantity +
-      "</td><td>"
-      "<input type='checkbox'/>"
-      "</td></tr>";
+      "</td><td>"+
+    '<input type="checkbox"/>';
+    "</td></tr>";
+  });
+  mytable += "</table>";
+  console.log(mytable);
+  document.getElementById("table1").innerHTML = mytable;
+  myDD="<select><option>--Select Product--</option>"
+  cart2.forEach((element) => {
+    myDD+="<option>"+element.Comp+"</option>";
+  });
+  myDD+="</select>"
+  document.getElementById('bill').innerHTML=myDD;
+
+  myDD2="<select><option>--Select Product--</option>"
+  cart2.forEach((element) => {
+    myDD2+="<option>"+element.Comp+"</option>";
+  });
+  myDD2+="</select>"
+  document.getElementById('Item').innerHTML=myDD2;
+
+  myDD3="<select><option>--Select Product--</option>"
+  cart2.forEach((element) => {
+    myDD3+="<option>"+element.Comp+"</option>";
+  });
+  myDD3+="</select>"
+  document.getElementById('sel').innerHTML=myDD3;
+ 
+}
+function display() {
+  mytable =
+    "<table><tr><th>Company</th><th>Model</th><th>Memory(GB)</th><th>Price(Rs)</th><th>Quantity</th><th>Action</th><th>Rating</th></tr>";
+
+  cart2.forEach((element) => {
+    mytable +=
+      "<tr><td>" +
+      element.Comp +
+      "</td><td>" +
+      element.Mod +
+      "</td><td>" +
+      element.Mem +
+      "</td><td>" +
+      element.Price +
+      "</td><td>" +
+      element.Quantity +
+      "</td><td>";+
+    '<input type="checkbox"/>' 
+    "</td></tr>";
   });
   mytable += "</table>";
   console.log(mytable);
   document.getElementById("table1").innerHTML = mytable;
 }
 
-// function compare(a, b) {
-//     cart2.sort(com);
-//     cart2.forEach((element) => {
-//         console.log("Name of the company" + element.Comp);
-//     });
-    
-// }
-// function com(a, b) {
-//     if (a.Comp < b.Comp) {
-//         return -1;
-//     }
-//     if (a.Comp > b.Comp) {
-//         return 1;
-//     }
-//     return 0;
-// }
-// cart2.sort(compare)
-//   tab=document.getElementById("table1") ;
+function sort() {
+  var st = document.getElementById("sort").value;
+  var sel = document.getElementById("sortby").value;
+  console.log("value of st " + st);
+  console.log("value of sel " + sel);
+  if (sel == "company") {
+    if (st == "ascending") {
+      cart2.sort((a, b) => {
+        if (a.Comp < b.Comp) {
+          return -1;
+        }
+        if (a.Comp > b.Comp) {
+          return 1;
+        }
+        return 0;
+      });
+    } else if (st == "descending") {
+      cart2.sort((a, b) => {
+        if (a.Comp > b.Comp) {
+          return -1;
+        }
+        if (a.Comp < b.Comp) {
+          return 1;
+        }
+        return 0;
+      });
+    }
+  } else if (sel == "model") {
+    if (st == "ascending") {
+      cart2.sort((a, b) => {
+        if (a.Mod < b.Mod) {
+          return -1;
+        }
+        if (a.Mod > b.Mod) {
+          return 1;
+        }
+        return 0;
+      });
+    } else if (st == "descending") {
+      cart2.sort((a, b) => {
+        if (a.Mod > b.Mod) {
+          return -1;
+        }
+        if (a.Mod < b.Mod) {
+          return 1;
+        }
+        return 0;
+      });
+    }
+  } else if (sel == "mem") {
+    if (st == "ascending") {
+      cart2.sort((a, b) => a.Mem - b.Mem);
+    } else if (st == "descending") {
+      cart2.sort((a, b) => b.Mem - a.Mem);
+    }
+  } else if (sel == "price") {
+    if (st == "ascending") {
+      cart2.sort((a, b) => a.Price - b.Price);
+    } else if (st == "descending") {
+      cart2.sort((a, b) => b.Price - a.Price);
+    }
+  }
 
-//   function compare1(a, b) {
-//     cart2.sort(mod);
-//     cart2.forEach((element) => {
-//         console.log("Name of the company" + element.Mod);
-//     });
-    
-// }
-// function mod(a, b) {
-//     if (a.Mod < b.Mod) {
-//         return -1;
-//     }
-//     if (a.Mod > b.Mod) {
-//         return 1;
-//     }
-//     return 0;
-// }
-// cart2.sort(mod)
-// tab1=document.getElementById("table1") ;
-
+  display();
+}
 
 var invoice = [];
 function add_cart() {
-  var des = document.getElementById("company").value;
-  var quanti = document.getElementById("quantity").value;
-  var am = document.getElementById("price").value;
+  var des = document.getElementById("bill").value;
+  var quanti = document.getElementById("quantityy").value;
+  var am = (document.getElementById("price").value) * (quanti);
 
   table2 =
     "<table><tr><th>Bill</th><tr><th>Description</th><th>Quantity</th><th>Amount(Rs)</th></tr>";
@@ -106,26 +178,23 @@ function add_cart() {
       "  " +
       "</td></tr>";
   });
-  table2 += "<tr><th>Total</th></tr></table>";
+  table2 += "</table>";
   document.getElementById("table2").innerHTML = table2;
 }
 
-// function compare( a, b ) {
-//     var cmp=document.getElementById("company").value;
-//     if ( a.cmp < b.cmp ){
-//       return -1;
-//     }
-//     if ( a.cmp > b.cmp ){
-//       return 1;
-//     }
-//     return 0;
-//   }
+function rate(){
+  var val=document.getElementById("rates").value;
+  
+}
 
-//   cart.sort( compare );
-//   document.getElementById("table").value;
+// function updateItems(){
+//   var Index=document.getElementById("Item").value;
+//   var NewQuantity=document.getElementById("new").value;
+//   var data=cart2[Index];
+//   console.log(cart2[0].Quantity);
+//   // forEach Loop
+//   data.Quantity=NewQuantity;
 
-// function ddlselect(){
-//     var d=document.getElementById("dselect");
-//     var displaytext=d.options[d.selectedIndex].text;
-//     document.getElementById("txtvalue").value=displaytext;
-//   
+//   display();
+// }
+
